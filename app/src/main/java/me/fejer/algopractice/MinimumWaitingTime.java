25e58c4a70139a00467ece38c47a553080902793
Ferenc Fejer <FerencFejer@gmail.com>
2021-03-26 09:05:27 +0100

    Min wait time desc

--- app/src/main/java/me/fejer/algopractice/MinimumWaitingTime.java
+++ app/src/main/java/me/fejer/algopractice/MinimumWaitingTime.java
@@ -8,6 +8,11 @@ public class MinimumWaitingTime {
         minimumWaitingTime(new int[]{1, 2, 3, 4});
     }
 
+    /*
+     *  the main idea here is to sort in ascending order. that way the queries with the lower latency are in front
+     *  thus the latter ones (the ones that are longer) wait less than if they were in the front.
+     *
+     * */
     public static int minimumWaitingTime(int[] queries) {
         Arrays.sort(queries);
         int runningSum = 0;
